74e2697840563a2b5cb80a843b6807aaf7967ad6
nvoter <avmanushkina@edu.hse.ru>
2025-01-13 11:18:53 +0300

    initial commit

---
+++ VerbiFrontend/VerbiFrontend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VerbiFrontend
+//
+//  Created by Анастасия Манушкина on 13.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
